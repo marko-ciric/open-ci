ff5502d67b81df3e198a429c7e84dd3e74f9e950
Marko Ciric <ciric.marko@gmail.com>
2014-04-24 00:31:10 +0200

    task save/update tests.

--- test/taskTests.js
+++ test/taskTests.js
@@ -2,13 +2,29 @@ var expect = require('chai').expect;
 var task = require('../lib/task');
 
 describe("task", function () {
-    describe("#create()", function () {
+    describe("create", function () {
         it("should exist", function () {
             expect(task.create).to.exist;
         });
         it("should create a task with id", function () {
             var t = task.create();
             expect(t.id).not.to.be.null;
+        });
+        it("should have a git repo", function () {
+            var t = task.create({repo: "git"});
+            expect(t.repo).to.be.null;
+        });
+    });
+
+    describe("save", function () {
+        it("should update git repo", function () {
+            var t = task.create({
+               id: "1", repo: "git://repo1.org"
+            });
+            expect(t.repo).to.equal("git://repo1.org");
+            t.repo = "git://repo2.org";
+            t = task.save(t);
+            expect(t.repo).to.equal("git://repo2.org");
         });
     });
 });

4ae47507fb8d2a698ac2fed92397febf340e1f24
Marko Ciric <ciric.marko@gmail.com>
2014-04-23 22:48:53 +0200

    Added RESTful for User domain.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var express = require('express');
 
 var routes = require('./routes');
 var user = require('./routes/user');
-var tasks = require('./routes/tasks');
+var task = require('./routes/task');
 
 var http = require('http');
 var path = require('path');
@@ -36,15 +36,17 @@ app.configure('production', function () {
     app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
+app.get('/user', user.list);
+app.get('/user/:id', user.get);
+app.put('/user', user.create);
+app.post('/user/:id', user.save);
+app.delete('/user/:id', user.remove);
 
-app.get('/users', user.list);
-
-app.get('/task', tasks.list);
-app.get('/task/:id', tasks.get);
-app.post('/task/:id', tasks.get);
-app.post('/task/:id/start', tasks.start);
-app.post('/task/:id/stop', tasks.stop);
+app.get('/task', task.list);
+app.get('/task/:id', task.get);
+app.post('/task/:id', task.get);
+app.post('/task/:id/start', task.start);
+app.post('/task/:id/stop', task.stop);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,23 @@
+var mongoose = require('mongoose');
+var winston = require('winston');
+var user = exports = module.exports = {};
 
-/*
- * GET users listing.
- */
+user.list = function (req, res) {
+    res.send("respond with a resource");
+};
 
-exports.list = function(req, res){
-  res.send("respond with a resource");
+user.get = function (req, res) {
+    res.send("respond with a resource");
+};
+
+user.create = function (req, res) {
+    res.send("respond with a resource");
+};
+
+user.save = function (req, res) {
+    res.send("respond with a resource");
+};
+
+user.remove = function (req, res) {
+    res.send("respond with a resource");
 };
